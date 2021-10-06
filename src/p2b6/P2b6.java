804fbf0b3b1f60c65296be19e5cfc4abd5da4f27
DELL <DELL@DESKTOP-OGRVV23>
2021-10-06 16:00:00 +0700

    p2b6

---
+++ src/p2b6/P2b6.java
@@ -0,0 +1,38 @@
+
+package p2b6;
+
+
+public class P2b6 {
+public static boolean isPrimeNumber(int n) {
+        
+        if (n < 2) {
+            return false;
+        }
+       
+        int squareRoot = (int) Math.sqrt(n);
+        for (int i = 2; i <= squareRoot; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public static void main(String[] args) {
+        
+        int count = 0;
+        System.out.println("Liet ke tat ca cac so nguyen to co 5 chu so:");
+        for (int i = 10001; i < 99999; i+=2) {
+            if (isPrimeNumber(i)) {
+                System.out.println(i);
+                count++;
+            }
+        }
+        System.out.println("Tổng các số nguyên tố có 5 chữ số là: " + count);
+ 
+    
+   
+      
+    }
+    
+}
